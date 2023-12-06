bf74b92e1c9b92e8c7c137d0c08cae5adb066d50
ANDREWRSTR <andregh1223@gmail.com>
2023-12-06 11:40:26 +0700

    coba

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', [KasirController::class, 'home']);
+
 
 
 Route::get('/login', [LoginController::class, 'login'])->name('login');
@@ -32,34 +32,40 @@ Route::get('/registrasi', [RegistrasiController::class, 'registrasi']);
 Route::post('/registrasi', [RegistrasiController::class, 'proses_registrasi']);
 
 
-Route::get('/pelanggan', [KasirController::class, 'pelanggan']);
 
-Route::get('/tambahproduk', [KasirController::class, 'tambahproduk']);
-Route::post('/tambahproduk', [KasirController::class, 'prosestambah']);
 
-Route::get('/tambahpelanggan', [KasirController::class, 'tambah_pelanggan']);
-Route::post('/tambahpelanggan', [KasirController::class, 'proses_tambah_pelanggan']);
-
-Route::get('/hapus_produk/{id}', [KasirController:: class, 'hapus']);
-
-Route::get('/update_produk/{id}', [KasirController::class, 'update']);
-Route::post('/update_produk/{id}', [KasirController::class, 'proses_update']);
-
-Route::get('/hapus_pelanggan/{id}', [KasirController:: class, 'hapuspelanggan']);
-
-Route::get('/update_pelanggan/{id}', [KasirController::class, 'updatepelanggan']);
-Route::post('/update_pelanggan/{id}', [KasirController::class, 'proses_update_pelanggan']);
-
-Route::get('/penjualan', [KasirController::class, 'penjualan']);
-
-Route::get('/detail_produk/{id}', [KasirController::class, 'detail']);
-
-Route::get('/detail_pelanggan/{id}', [KasirController::class, 'detailpelanggan']);
-
-Route::get('/logout',[LoginController::class,'logout']);
 
  Route::middleware(['auth'])->group(function(){
+    Route::get('/home', [KasirController::class, 'home']);
+
     Route::get('/produk', [KasirController::class, 'produk']);
+
+    Route::get('/tambahproduk', [KasirController::class, 'tambahproduk']);
+    Route::post('/tambahproduk', [KasirController::class, 'prosestambah']);
+
+    Route::get('/detail_produk/{id}', [KasirController::class, 'detail']);
+
+    Route::get('/update_produk/{id}', [KasirController::class, 'update']);
+    Route::post('/update_produk/{id}', [KasirController::class, 'proses_update']);
+
+    Route::get('/hapus_produk/{id}', [KasirController:: class, 'hapus']);
+
+    Route::get('/pelanggan', [KasirController::class, 'pelanggan']);
+
+    Route::get('/tambahpelanggan', [KasirController::class, 'tambah_pelanggan']);
+    Route::post('/tambahpelanggan', [KasirController::class, 'proses_tambah_pelanggan']);
+
+    Route::get('/detail_pelanggan/{id}', [KasirController::class, 'detailpelanggan']);
+
+    Route::get('/update_pelanggan/{id}', [KasirController::class, 'updatepelanggan']);
+    Route::post('/update_pelanggan/{id}', [KasirController::class, 'proses_update_pelanggan']);
+
+    Route::get('/hapus_pelanggan/{id}', [KasirController:: class, 'hapuspelanggan']);
+
+    Route::get('/penjualan', [KasirController::class, 'penjualan']);
+
+    Route::get('/logout',[LoginController::class,'logout']);
+
 
  });
 
